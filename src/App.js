af84988a813830f2223c209f9933204b24096bf8
Salrabber <salrabber@gmail.com>
2022-06-13 17:32:54 +0300

    Something went wrong

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
+import AddTodo from './Todo/AddTodo'
 
 function App() {
+  let [todos, setTodos] = React.useState([
+    { id: 1, completed: false, title: "Подрочить" },
+    { id: 2, completed: false, title: "Купить гандон" },
+    { id: 3, completed: false, title: "Купить шлюху" },
+  ]);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id){
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      completed: false,
+    }]))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{removeTodo}}>
+      <div className="wrapper">
+        <h1>React tutor</h1>
+        <AddTodo onCreate={addTodo} />
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : <p>Nu ti i bidlo</p>}
+      </div>
+    </Context.Provider>
   );
 }
 
