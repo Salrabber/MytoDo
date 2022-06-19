bc852329275fac4d7db4de68e3ad7e12871a1127
Salrabber <salrabber@gmail.com>
2022-06-19 14:01:34 +0300

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
-import AddTodo from "./Todo/AddTodo";
 import Loader from "./loader";
+
+const AddTodo = React.lazy(() => import("./Todo/AddTodo"));
 
 function App() {
   let [todos, setTodos] = React.useState([]);
@@ -14,7 +15,7 @@ function App() {
       .then((todos) => {
         setTimeout(() => {
           setTodos(todos);
-          setLoading(false)
+          setLoading(false);
         }, 2000);
       });
   }, []);
@@ -23,7 +24,9 @@ function App() {
     fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
       .then((response) => response.json())
       .then((todos) => {
-        setTimeout(() => {setTodos(todos)}, 2000)
+        setTimeout(() => {
+          setTodos(todos);
+        }, 2000);
       });
   }, []);
 
@@ -58,7 +61,9 @@ function App() {
     <Context.Provider value={{ removeTodo }}>
       <div className="wrapper">
         <h1>React tutor</h1>
-        <AddTodo onCreate={addTodo} />
+        <React.Suspense fallback={<p>Ждем твою мамку....</p>}>
+          <AddTodo onCreate={addTodo} />
+        </React.Suspense>
 
         {loading && <Loader />}
 
